15a5e4ecc32805b0c04418eccfd329292d89c771
LongHoang <longhoang@gmail.com>
2024-03-21 15:41:23 -0700

    implement socket

--- src/services/WsService.ts
+++ src/services/WsService.ts
@@ -1,15 +1,35 @@
 import { validateTokenWS } from './../core/JWT';
 class WsService {
-  static authenticate(connection: any, data: { accessToken: string }): void {
-    const { accessToken } = data;
-    const decode = validateTokenWS('ACCESS', accessToken);
-    if (!decode) {
-      connection.close();
-      return;
+  static socket: WebSocket | null = null;
+
+  static onConnection(socket: any): void {
+    WsService.socket = socket;
+    socket.on('message', WsService._onMessage);
+    socket.on('close', WsService._onClose);
+  }
+
+  static _onMessage(message: string): void {
+    const { accessToken, type, data } = JSON.parse(message);
+    if (!accessToken) {
+      console.log('accessToken not found');
+      WsService?.socket?.close();
     }
-    console.log('Client connected');
-    connection.decode;
-    connection.send("You're account is verified");
+    const user = validateTokenWS('ACCESS', accessToken);
+    switch (type) {
+      case 'INIT':
+        console.log('user', user, type, data);
+        break;
+      default:
+        break;
+    }
+  }
+
+  static _onClose(): void {
+    WsService.sendData('client actively close successfully');
+  }
+
+  static sendData(data: string) {
+    WsService.socket?.send(data);
   }
 }
 
