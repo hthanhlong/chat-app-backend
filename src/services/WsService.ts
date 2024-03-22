8f4a8c3d086dc0b01a5cd8a19ca8f2a19a480020
LongHoang <longhoang@gmail.com>
2024-03-22 00:40:46 -0700

    fix connect socket

--- src/services/WsService.ts
+++ src/services/WsService.ts
@@ -6,11 +6,16 @@ class WsService {
   static onConnection(socket: any): void {
     WsService.socket = socket;
     socket.on('message', WsService._onMessage);
+    socket.on('close', WsService._onClose);
+    socket.on('error', (err: any) => {
+      console.error('Socket error', err);
+    });
   }
 
   static _onMessage(message: string): void {
+    // console.log('======>');
     const { accessToken, data } = JSON.parse(message);
-    if (!accessToken) return WsService?.socket?.close();
+    if (!accessToken) return;
     const user = validateTokenWS('ACCESS', accessToken) as {
       id: string;
       username: string;
@@ -18,6 +23,11 @@ class WsService {
     const { type } = data;
     switch (type) {
       case 'INIT':
+        // console.log(user.id, user.username, 'connected');
+        // @ts-ignore
+        WsService.socket.clientId = user.id;
+        // @ts-ignore
+        WsService.socket.username = user.username;
         WsService.clients[user.id] = { id: user.id, socket: WsService.socket };
         break;
       default:
@@ -25,9 +35,23 @@ class WsService {
     }
   }
 
+  static _onClose(): void {
+    // get close from client
+    // @ts-ignore
+    const { clientId } = WsService.socket;
+    if (clientId) {
+      // console.log(clientId, username, 'disconnected');
+      delete WsService.clients[clientId];
+      if (WsService.socket) {
+        WsService.socket.close();
+      }
+    }
+  }
+
   static sendDataToClientById({ id, data }: sendDataToIdByWs) {
     const client = WsService.clients[id];
     if (!client) return;
+    // console.log('client.id', client.id);
     client.socket.send(JSON.stringify(data));
   }
 }
