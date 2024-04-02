bf1f475ccfc7c95fa42ff7804fac3a3173bd0ecd
LongHoang <longhoang@gmail.com>
2024-04-02 00:43:23 -0700

    add dotenv

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,7 @@
 // Mapper for environment variables
+import dotenv from 'dotenv'
+dotenv.config()
+
 export const environment = process.env.NODE_ENV
 export const port = process.env.PORT
 export const timezone = process.env.TZ
