1885f1bb35cb052543b34263a37ff6f12f8ee87d
LongHoang <longhoang@gmail.com>
2024-04-01 23:45:05 -0700

    fix wrong name class

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,10 +1,10 @@
 import { dataSelectedByKeys } from '../database/utils'
 import User from '../database/model/User'
-import userRepository from '../repositories/UserRepository'
+import UserRepository from '../repositories/UserRepository'
 
 class UserService {
   async getAllUsers(id: string) {
-    const result = await userRepository.getAllUsers(id)
+    const result = await UserRepository.getAllUsers(id)
     const response = dataSelectedByKeys(result.users, [
       '_id',
       'username',
@@ -16,19 +16,19 @@ class UserService {
   }
 
   async updateUserById(id: string, newDataOfUser: User) {
-    const result = await userRepository.updateUserById(id, newDataOfUser)
+    const result = await UserRepository.updateUserById(id, newDataOfUser)
     if (!result.user) return null
     return result.user
   }
 
   async findUserByEmail(email: string): Promise<User | null> {
-    const result = await userRepository.findUserByEmail(email)
+    const result = await UserRepository.findUserByEmail(email)
     if (!result.user) return null
     return result.user
   }
 
   async findUserById(id: string) {
-    const result = await userRepository.findUserById(id)
+    const result = await UserRepository.findUserById(id)
     if (!result.user) return null
     const response = dataSelectedByKeys(result.user, [
       '_id',
@@ -41,7 +41,7 @@ class UserService {
   }
 
   async findUserByUsername(username: string) {
-    const result = await userRepository.findUserByUsername(username)
+    const result = await UserRepository.findUserByUsername(username)
     if (!result.user) return null
     return result.user
   }
