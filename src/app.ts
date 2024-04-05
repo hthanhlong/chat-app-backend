298789f9ce6f6464c6803afc8f6818fb4e90564a
LongHoang <longhoang@gmail.com>
2024-04-04 17:23:26 -0700

    fix CORS error

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ const limiter = rateLimit(rateLimitOptions)
 app.use(limiter)
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded(urlConfigEncode))
-app.use(cors({ origin: '*' }))
+app.use(cors())
 app.use(helmet())
 //routes
 app.use('/api/v1', routes)
